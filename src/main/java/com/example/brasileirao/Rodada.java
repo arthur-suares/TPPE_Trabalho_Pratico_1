5423b3cee50ca33a38d25465b9094b348fed3acb
cqcoding <ceciquaresma2014@gmail.com>
2025-10-17 11:43:15 -0300

    [feat] adicionando a classe Rodada

---
+++ src/main/java/com/example/brasileirao/Rodada.java
@@ -0,0 +1,26 @@
+package com.example.brasileirao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Rodada {
+    private final int numero;
+    private final List<Partida> partidas = new ArrayList<>(); //lista de partidas da rodada ex: (fla, flu, 3, 2)
+    
+    public Rodada(int numero) { //numero da rodada
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void adicionarPartida(Partida p) {
+        partidas.add(p);
+    }
+
+    public List<Partida> getPartidas() {
+        return Collections.unmodifiableList(partidas);
+    }
+}
