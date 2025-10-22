42124825c72a9b38ea63a85827ec242620600ad4
Marianannn <mariana.leticia05@gmail.com>
2025-10-21 22:21:33 -0300

    test: adicionando suites de testes e categoria funcional

---
+++ src/test/java/com/example/brasileirao/AllTests.java
@@ -0,0 +1,11 @@
+package com.example.brasileirao;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SteFuncional.class})
+public class AllTests {
+    
+}
